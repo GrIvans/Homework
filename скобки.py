968e5dda22fd023a8714b2bdc2a6492f8f39dbe5
GrIvans <gribkovivan25@ya.ru>
2020-11-28 16:59:20 +0300

    сделал задачу про скобки

---
+++ скобки.py
@@ -0,0 +1,35 @@
+s = open('input1.txt', encoding='utf-8').read()
+for i in range(len(s)):
+    p = s.find('(')
+    if p != -1:
+        p1 = s.rfind(')')
+        if p1 > p and p1 != -1:
+            s = s.replace('(', '', 1)
+            s = s.replace(')', '', 1)
+    p = s.find('[')
+    if p != -1:
+        p1 = s.rfind(']')
+        if p1 > p and p1 != -1:
+            s = s.replace('[', '', 1)
+            s = s.replace(']', '', 1)
+    p = s.find('{')
+    if p != -1:
+        p1 = s.rfind('}')
+        if p1 > p and p1 != -1:
+            s = s.replace('{', '', 1)
+            s = s.replace('}', '', 1)
+if s == '':
+    print('YES')
+else:
+    if s.find('(') != -1:
+        print('Лишняя скобка -- "("')
+    if s.find('(') != -1:
+        print('Лишняя скобка -- ")"')
+    if s.find('(') != -1:
+        print('Лишняя скобка -- "["')
+    if s.find('(') != -1:
+        print('Лишняя скобка -- "]"')
+    if s.find('(') != -1:
+        print('Лишняя скобка -- "{"')
+    if s.find('(') != -1:
+        print('Лишняя скобка -- "}"')
