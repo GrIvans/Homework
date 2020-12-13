997999da2a224f1f9bb1f72ae19cdef90102a646
GrIvans <gribkovivan25@ya.ru>
2020-12-13 22:13:17 +0300

    13.12

--- ege24/#135.py
+++ ege24/#135.py
@@ -1,7 +1,8 @@
-# не работает
-s = open('input.txt').read()
-s = s.replace('JBOSS', ' ')
-s = s.replace('BOSSJ', ' ')
-s = s.replace('JBOSSJ', ' ')
-s = s.replace('BOSS', '1')
-print(s.count('1'))
+s = open('24data/24-j4.txt').read()
+s += ' '
+k = 0
+for i in range(1, len(s) - 4):
+    if s[i] == 'B' and s[i + 1] == 'O' and s[i + 2] == 'S' and s[i + 3] == 'S' and\
+        s[i - 1] != 'J' and s[i + 4] != 'J':
+        k += 1
+print(k)
--- ege24/#144.py
+++ ege24/#144.py
@@ -1,11 +1,12 @@
-s = open('input.txt').read()
+s = open('24data/24-j6.txt').read()
+s += '0'
 k = 0
 n = s[0]
 for i in range(1, len(s)):
-    if s[i] > s[i - 1]:
+    if (s[i]) > (s[i - 1]):
         n += s[i]
     else:
         if len(n) == 5:
             k += 1
-        n = ''
+        n = s[i]
 print(k)
