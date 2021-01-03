984badcaa7fdd4c116a7ce83cc4709b8fbd2d1b5
GrIvans <gribkovivan25@ya.ru>
2021-01-03 21:02:10 +0300

    сделал 6

--- ege27/#6.py
+++ ege27/#6.py
@@ -1,15 +1,21 @@
-#data = open('27data/6/27-6a.txt').readlines()
-data = open('27data/6/27-6b.txt').readlines()
+data = open('27data/6/27-6a.txt').readlines()
+#data = open('27data/6/27-6b.txt').readlines()
 #data = open('input.txt').readlines()
-#1--782040 верно
-#2--
+# 1--782040 верно
+# 2--997002 верно
 kolvo = int(data[0])
 data = data[1:]
 data = sorted(list(map(int, data)), reverse=1)
-r = []
-for i in range(len(data)):
-    for n in range(i + 1, len(data)):
-        R = (data[i] * data[n])
+rmax = 0
+left_gran = len(data)
+i = 0
+while i <= left_gran:
+    n = i + 1
+    while n <= left_gran:
+        R = data[i] * data[n]
         if R % 6 == 0:
-            r.append(R)
-print(sorted(r, reverse=1)[0])
+            rmax = max(rmax, R)
+            left_gran = n
+        n += 1
+    i += 1
+print(rmax)
