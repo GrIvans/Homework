790b62cb08dbc3b7af22cd343a302958ff35a359
GrIvans <gribkovivan25@ya.ru>
2021-01-13 11:58:55 +0300

    сделал 31

--- ege25/#31.py
+++ ege25/#31.py
@@ -1,11 +1,15 @@
-def f(N):
-    D = []
-    if (N*0.5).is_integer():
-        D = [d for d in range (1, N // 2 + 1) if N % d == 0] + [N]
-    return D
+def f(a):
+    n = (a**0.5)
+    m = []
+    if n.is_integer():
+        for i in range(1, int(n) + 1):
+            if a % i == 0:
+                m.append(i)
+                if a // i != i:
+                    m.append(a // i)
+    return m
 
 for i in range(1820348, 2880927 + 1):
     a = f(i)
     if len(a) == 5:
         print(a)
-
