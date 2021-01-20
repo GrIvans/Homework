b96f07219df9f1935df2d6b6eb8e8936208ca811
GrIvans <gribkovivan25@ya.ru>
2021-01-20 23:06:20 +0300

    20.1

---
+++ ege15/#388.py
@@ -0,0 +1,4 @@
+for a in range(1, 121):
+    fl = True
+    for x in range(1, 10000):
+        pass
---
+++ ege15/#389.py
@@ -0,0 +1,8 @@
+for a in range(1, 22):
+    fl = True
+    for x in range(1, 1205):
+        if (21 % a == 0 and (x % 40 != 0 or x % 30 != 0 or x % a == 0)) == 0:
+            fl = False
+            break
+    if fl:
+        print(a)
