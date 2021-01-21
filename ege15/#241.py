4c9ebcc62cd04779a41d9fbb9dad7bfa846d6a0e
GrIvans <gribkovivan25@ya.ru>
2021-01-21 22:49:12 +0300

    21ю1

---
+++ ege15/#241.py
@@ -0,0 +1,9 @@
+for a in range(1, 1001):
+    fl = True
+    for x in range(1, 1001):
+        for y in range(1, 1001):
+            if (not(x <= 5) or (x * x <= a)) and (not(y * y <= a) or y < 7) == 0:
+                fl = False
+    if fl:
+        print(a)
+#ans--48 верно
