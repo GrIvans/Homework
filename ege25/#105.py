859d3c1c0fc586305cb6c832e51d877a580dc972
GrIvans <gribkovivan25@ya.ru>
2021-02-28 15:27:20 +0300

    28,02

--- ege25/#105.py
+++ ege25/#105.py
@@ -8,27 +8,18 @@ def prost(a):
 def delit(a):
     n = int(a**.5)
     mn = 100001
+    m = ()
     for i in range(1, n + 1):
         if a % i == 0:
-            if prost(i) and prost(a // i):
-                if mn < a // i - i:
+            if prost(i) and prost(a // i) and i != a // i:
+                if mn > a // i - i:
                     mn = a // i - i
-    return mn
+                    m = (i, a // i)
+    return (mn, m)
 
 
-def dl(a):
-    m = []
-    for i in range(1, int(a**.5) + 1):
-        if a % i == 0:
-            m.append(i)
-            if a // i != i:
-                m.append(a // i)
-    return sorted(m)
-
-
-mnc = []
+an = []
 for i in range(523456, 578926):
-    s = delit(i)
-    mnc.append((s, i))
-mnc.sort()
-print(dl(mnc[0][1]))
+    an.append(delit(i))
+an.sort()
+print(*an[0][1])
